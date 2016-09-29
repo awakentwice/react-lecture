4b571d199d3fa79b8958d70045a24a944d77ac7d
Маушов Динислам Каримуллаевич <dkmaushov@sbn-mac-24.local>
2016-09-29 14:24:23 +0300

    first commit

---
+++ contacts.js
@@ -0,0 +1,23 @@
+var CONTACTS = [
+  {
+    id: 1,
+    name: 'Darth Vader',
+    number: '+250966666666',
+    imgSrc: 'http://cs7.pikabu.ru/images/big_size_comm_an/2014-03_7/13962622876915.gif'
+  }, {
+    id: 2,
+    name: 'Princess Leia',
+    number: '+250966344466',
+    imgSrc: 'http://images6.fanpop.com/image/photos/33100000/CARRIE-FISHER-anakin-vader-and-princess-leia-33186069-190-149.gif'
+  }, {
+    id: 3,
+    name: 'Luke Skywalker',
+    number: '+250976654433',
+    imgSrc: 'http://www.reactiongifs.us/wp-content/uploads/2013/05/nooooooo_luke_skywalker.gif'
+  }, {
+    id: 4,
+    name: 'Chewbacca',
+    number: '+250456784935',
+    imgSrc: 'https://media.giphy.com/media/RUUdVZqwpfTRS/giphy.gif'
+  }
+]
